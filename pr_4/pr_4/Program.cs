22b3407dd55b72b6c8d640baa7088744ef4c1acd
NILT <100668725+NILT-Of@users.noreply.github.com>
2024-04-14 19:20:44 +0300

    Add files via upload

---
+++ pr_4/pr_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class pr_4
+
+{
+    static void Main()
+    {
+        int sotchik = 0;
+        int cyma = 0;
+        Console.WriteLine("Введите количество чисел в массиве: ");
+        int O = Convert.ToInt32(Console.ReadLine());
+        int[] maseve = new int[O];
+        Console.WriteLine("Введите {0} чисел: ", O);
+        for (int i = 0; i < O; i++)
+        {
+            maseve[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        foreach (var chislo in maseve)
+        {
+            if (chislo % 2 != 0)
+            {
+                ++sotchik;
+                cyma += chislo;
+            }
+        }
+        if (sotchik == 0)
+        {
+            Console.WriteLine("Нет нечётных чисел (");
+        }
+        else
+        {
+            Console.WriteLine("Среднее арифметическое нечётных чисел: \n{0}", (float)cyma / sotchik);
+        }
+    }
+}
---
+++ pr_5/pr_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class pr_5
+{
+    static void Main(string[] args)
+    {
+        // Вводим размер массива
+        Console.Write("Введите размер массива (N): ");
+        int N = int.Parse(Console.ReadLine());
+
+        // Создание массива и ввод чисел
+        int[] numbers = new int[N];
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < N; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Вывод элементов в обратном порядке
+        Console.WriteLine("Массив в обратном порядке:");
+        for (int i = N - 1; i >= 0; i--)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
