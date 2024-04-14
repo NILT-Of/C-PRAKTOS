ff2b9e18a8fb18070671c522d09cadf775cea08a
NILT <100668725+NILT-Of@users.noreply.github.com>
2024-04-14 19:25:21 +0300

    Add files via upload

---
+++ pr_14/pr_14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class pr_14
+{
+    static double RootK(double X, int K, int N)
+    {
+        if (N == 0)
+        {
+            return 1; //Y0 = 1
+        }
+        else
+        {
+            double YPrev = RootK(X, K, N - 1);
+            return YPrev - (YPrev - X / Math.Pow(YPrev, K - 1)) / K;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        double X = 16; // пример числа
+        int K = 2; // пример степени
+        int[] N_values = { 1, 2, 3, 4, 5, 6 }; // пример значений
+
+        foreach (int N in N_values)
+        {
+            double root = RootK(X, K, N);
+            Console.WriteLine($"При N = {N}, приближенное значение корня {K}-й степени из числа {X}: {root}");
+        }
+    }
+}
---
+++ pr_15/pr_15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Lecarstvo
+{ 
+    public string Name { get; set; }
+    public string Manufacturer { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    public DateTime ExpiryDate { get; set; }
+
+    public Lecarstvo(string name, string manufacturer, int quantity, decimal price, DateTime expiryDate)
+    {
+        Name = name;
+        Manufacturer = manufacturer;
+        Quantity = quantity;
+        Price = price;
+        ExpiryDate = expiryDate;
+    }
+}
+
+class pr_15
+{
+    static void Main(string[] args)
+    {
+        Lecarstvo medication1 = new Lecarstvo("Аспирин", "Bayer", 1488, 2.5m, new DateTime(2024, 12, 31));
+        Lecarstvo medication2 = new Lecarstvo("Но-шпа", "Sanofi", 228, 3.0m, new DateTime(2025, 6, 30));
+
+        Console.WriteLine("Информация о лекарственных препаратах:");
+        Console.WriteLine($"Препарат: {medication1.Name}, Производитель: {medication1.Manufacturer}, Количество: {medication1.Quantity}, Цена: {medication1.Price}, Срок годности: {medication1.ExpiryDate.ToShortDateString()}");
+        Console.WriteLine($"Препарат: {medication2.Name}, Производитель: {medication2.Manufacturer}, Количество: {medication2.Quantity}, Цена: {medication2.Price}, Срок годности: {medication2.ExpiryDate.ToShortDateString()}");
+    }
+}
